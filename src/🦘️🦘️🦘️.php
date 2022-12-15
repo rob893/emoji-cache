7fcd35cecc7506258b91e4e95284582bdd008cde
Michael Strelan <mstrelan@gmail.com>
2022-12-15 13:28:01 +1000

    Add PSR-4 autoloading, composer.json and test coverage

---
+++ src/🦘️🦘️🦘️.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace rob893\EmojiCache;
+class 🦘️🦘️🦘️
+{
+    public $🔑;
+
+    public $🐬;
+
+    public ?🦘️🦘️🦘️ $👉;
+
+    public ?🦘️🦘️🦘️ $👈;
+
+    public function __construct($🔑, $🐬, ?🦘️🦘️🦘️ $👉 = null, ?🦘️🦘️🦘️ $👈 = null)
+    {
+        $this->🔑 = $🔑;
+        $this->🐬 = $🐬;
+        $this->👉 = $👉;
+        $this->👈 = $👈;
+    }
+}
---
+++ tests/🧑‍🔬🧪Test.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use rob893\EmojiCache\🐆🐆🐆;
+
+class 🧑‍🔬🧪Test extends TestCase {
+
+  public function test☢️☢️☢️️(): void {
+    $😈 = new 🐆🐆🐆();
+
+    $🔑 = 'foo';
+    $⚰️ = 12345;
+
+    // Check if cache has key. Will be false.
+    $😍 = $😈->🤔🤔🤔($🔑);
+    $this->assertFalse($😍);
+
+    // Set key value.
+    $😈->✍️✍️✍️($🔑, $⚰️);
+
+    // Check if key exists. Will be true.
+    $💣 = $😈->🤔🤔🤔($🔑);
+    $this->assertTrue($💣);
+
+    // Get value for key.
+    $⚔️ = $😈->🤔🤞😃($🔑);
+    $this->assertEquals($⚰️, $⚔️);
+
+    // Get size of cache.
+    $📈 = $😈->👙💩🖇();
+    $this->assertEquals(1, $📈);
+
+    // Clear cache.
+    $😈->❌❌❌();
+
+    // Check size again.
+    $this->assertEquals(0, $😈->👙💩🖇());
+  }
+
+}
